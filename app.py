6fa53d1cf2098f398ae43cbefb2850c8cb78e355
xkennie <120515141+xkennie@users.noreply.github.com>
2025-05-07 21:12:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 #import matplotlib.pyplot as plt
+import scikit_learn as sklearn
 from sklearn.model_selection import train_test_split
 #from ydata_profiling import ProfileReport
 from sklearn.metrics import precision_score, recall_score
