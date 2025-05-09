0bb3f060efb080a75efb28865561079ca4896371
xkennie <120515141+xkennie@users.noreply.github.com>
2025-05-09 15:31:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -310,10 +310,10 @@ for name in selected_function_names:
         default_val = "" if param.default is param.empty else param.default
         input_val = st.text_input(f"{name} - {param.name}", value=str(default_val))
         params[param.name] = eval(input_val)
-    params.append(X_train)
-    params.append(X_test)
-    params.append(y_train)
-    params.append(y_test)
+    params["X_train"] = X_train 
+    params["X_test"] = X_test 
+    params["y_train"] = y_train 
+    params["y_test"] = y_test 
     func_params[name] = params
     
 
