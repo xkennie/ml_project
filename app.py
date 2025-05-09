f7e90b1aa7b3b4dc12ad5ee73deb8dae38a21fbc
xkennie <120515141+xkennie@users.noreply.github.com>
2025-05-09 14:05:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ st.title("Загрузи файл с пивой сюда, друг")
 uploaded_file = st.file_uploader("Select a CSV file", type=["csv"])
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  df.head()
+  st.write("Твой датасет:")
   st.write(df.head())
-  print(df.head())
+ 
 #чтение данных
 def read_data(df):
   for column in ['Size(L)',
