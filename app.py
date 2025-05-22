d2d00ea1ada704e82ae6f14f0817549c715259a2
xkennie <120515141+xkennie@users.noreply.github.com>
2025-05-23 01:09:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -669,7 +669,7 @@ def plot_confusion_matrix(y_true, y_pred, title):
     cm = confusion_matrix(y_true, y_pred)
     #st.write(cm)
     #fig, ax = plt.subplots(figsize=(10, 12))
-    sns.heatmap(cm, annot=cm, cbar=False, square = True, fmt='.f1', ax=ax)
+    sns.heatmap(cm, annot=True, cbar=False, square = True, fmt='.f1', ax=ax)
     ax.set_title(title)
     ax.set_xlabel('Predicted')
     ax.set_ylabel('Actual')
