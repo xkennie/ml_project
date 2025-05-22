5ed652ee08d4528fec87365adba7e2f7d368b636
xkennie <120515141+xkennie@users.noreply.github.com>
2025-05-23 00:48:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -915,10 +915,10 @@ with col2:
     st.dataframe(metrics_df)
 
     st.subheader("Train predictions")
-    st.dataframe(y_train_pred.head())
+    st.dataframe(pd.Series(y_train_pred.head()))
 
     st.subheader("Test predictions")
-    st.dataframe(y_test_pred.head())
+    st.dataframe(pd.Series(y_test_pred.head()))
 
 with col3:
   st.subheader("Random Forest")
