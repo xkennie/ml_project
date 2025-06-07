cc243314a8e4abad1a45e4d0cb702e715588dadd
xkennie <120515141+xkennie@users.noreply.github.com>
2025-06-07 11:44:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -622,8 +622,11 @@ if st.checkbox("Подготовить данные"):
         st.subheader("Результаты предобработки")
         st.header("Детальный анализ")
         st.subheader(f"Анализ по классам ({target_col})")
-        class_stats = df.groupby(target_col,as_index=False).mean().merge(pd.DataFrame(df[target_col].value_counts()).reset_index(), how='left')
-        st.dataframe(class_stats)
+        try:
+            class_stats = df.groupby(target_col,as_index=False).mean().merge(pd.DataFrame(df[target_col].value_counts()).reset_index(), how='left')
+            st.dataframe(class_stats)
+        except: 
+            continue
         col1, col2 = st.columns(2)
         with col1:
             st.metric("Обучающая выборка", f"{len(X_train)} наблюдений")
